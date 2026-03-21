d38069aa9bd80eb4c86a85a4129bd49117f0da76
AniketKarse <aniketkarse712@gmail.com>
2026-03-21 11:20:13 +0000

    chore: programaticNavigation

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,18 @@
 <script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
 
 const props = defineProps(['event']); //the short way
 
+const register = () => {
+    router.push({
+        name: 'EventDetails'
+    })
+}
 </script>
 
 <template>
     <p>Register for the event</p>
+    <button @click="register">Register me!</button>
 </template>
