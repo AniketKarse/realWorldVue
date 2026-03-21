6e2ce8ed509052001e6f4f78d084d9101e5bc0ac
AniketKarse <aniketkarse712@gmail.com>
2026-03-21 10:00:38 +0000

    chore : buildingPagination

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,25 +1,55 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch, computed } from 'vue';
 import EventCard from '@/components/EventCard.vue';
 import EventService from '@/services/EventService.js'
+import { RouterLink } from 'vue-router'
 
 const events = ref(null);
 
-onMounted(() => {
-  EventService.getEvents()
+const props = defineProps(['page'])
+const totalEvents = ref(0)
+const fetchevents = () => {
+  EventService.getEvents(2, props.page)
     .then((response) => {
       events.value = response.data
+      totalEvents.value = response.headers["x-total-count"]
     })
     .catch((error) => {
       console.log(error)
     })
+}
+
+const hasNext = computed(() => {
+  const totalPages = Math.ceil(totalEvents.value / 2)
+  return props.page < totalPages
 })
+
+
+onMounted(() => {
+  fetchevents()
+})
+
+watch(
+  () => props.page,
+  () => {
+    events.value = null
+    fetchevents()
+  }
+)
 </script>
 
 <template>
   <div class="events">
     <h1>Events for good</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div class="pagination">
+      <RouterLink id="page-prev" :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev" v-if="page != 1">
+        &#60; Previous</RouterLink>
+
+      <RouterLink id="page-next" :to="{ name: 'event-list', query: { page: page + 1 } }" rel="next" v-if="hasNext">Next
+        &#62;
+      </RouterLink>
+    </div>
   </div>
 </template>
 
@@ -28,5 +58,24 @@ onMounted(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
